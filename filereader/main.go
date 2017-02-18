ef6197bb8a893a0ab58d1f5b184890742ae7fcd2
mlampedx <mlampesd@gmail.com>
2017-02-17 21:30:41 -0800

    Wrote filereader using io/ioutil, os, fmt, and strings packages

---
+++ filereader/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// @desc main reads files and prints the number of instances of duplicate lines as well as their contents
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
