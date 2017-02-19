fe30d4506f4a0f2009e862039cddceb2969cda86
mlampedx <mlampesd@gmail.com>
2017-02-19 14:15:13 -0800

    Added gif route

--- echoserver/main.go
+++ echoserver/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 	"sync"
 )
 
-// @desc echoserver provides a count route, logger route, and echoes all other routes
+// @desc echoserver provides a count route, logger route, lissajous (gif) route, and echoes all other routes
 
 var mu sync.Mutex
 var count int
@@ -16,6 +22,9 @@ func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
 	http.HandleFunc("/logger", logger)
+	http.HandleFunc("/lissajous", func(w http.ResponseWriter, r *http.Request) {
+		lissajous(w)
+	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -30,6 +39,39 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "Count %d\n", count)
 	mu.Unlock()
+}
+
+func lissajous(out io.Writer) {
+	var palette = []color.Color{color.White, color.Black}
+
+	const (
+		whiteIndex = 0 // First color in palette
+		blackIndex = 1 // Second color in palette
+	)
+
+	const (
+		cycles  = 5     // Number of complete x oscillator revolutions
+		res     = 0.001 // Angular resolution
+		size    = 100   // Image canvas covers [-size..+size]
+		nframes = 64    // Number of animation frames
+		delay   = 8     // Delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // Relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // Phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
 }
 
 func logger(w http.ResponseWriter, r *http.Request) {
