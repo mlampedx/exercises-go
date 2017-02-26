5872d27a3c92792d1b11aa96594e0d4b26325df5
mlampedx <mlampesd@gmail.com>
2017-02-25 20:55:47 -0800

    Declared unit types and print funcs

---
+++ unitconv/unitconv.go
@@ -0,0 +1,42 @@
+package unitconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+type Meters float64
+type Feet float64
+type Kilometers float64
+type Miles float64
+type Pounds float64
+type Kilograms float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g°F", f)
+}
+
+func (m Meters) String() string {
+	return fmt.Sprintf("%g m", m)
+}
+
+func (ft Feet) String() string {
+	return fmt.Sprintf("%g ft", ft)
+}
+
+func (lbs Pounds) String() string {
+	return fmt.Sprintf("%g lbs", lbs)
+}
+
+func (kg Kilograms) String() string {
+	return fmt.Sprintf("%g kg", kg)
+}
