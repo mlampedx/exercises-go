e69771428e860e9bff6773110cd297459c92f3aa
mlampedx <mlampesd@gmail.com>
2017-05-03 21:05:15 -0700

    Added stack

---
+++ algorithms/stack/stack.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// stack implementation in go
+
+func main() {
+	s := []int{0}
+	s = push(s, 1)
+	s = push(s, 2)
+	s = push(s, 3)
+	s = push(s, 4)
+	s = push(s, 5)
+	s = pop(s)
+	s = remove(s, 4)
+	fmt.Printf("%v\n", s) // [0, 1, 2, 3]
+}
+
+func push(stack []int, i int) []int {
+	return append(stack, i)
+}
+
+func pop(stack []int) []int {
+	return stack[:len(stack)-1]
+}
+
+func remove(stack []int, i int) []int {
+	copy(stack[:i], stack[i+1:])
+	fmt.Println(stack)
+	return stack[:len(stack)-1]
+}
