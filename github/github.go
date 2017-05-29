32180c574e63bd5216991996b4212db3e66c5cb9
mlampedx <mlampesd@gmail.com>
2017-05-29 11:30:00 -0700

    Created IssuesSearchResult, Issue, and User types; SearchIssues function

---
+++ github/github.go
@@ -0,0 +1,54 @@
+package main
+
+// Go API for the GitHub issue tracker
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const endpointURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	Title     string
+	HTMLURL   string `json:"html_url"`
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+// SearchIssues queries the GitHub issue tracker
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(endpointURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
