a4e4b56d821994fb30b237eb55a1715d05ee2761
mlampedx <mlampesd@gmail.com>
2017-06-24 13:46:14 -0700

    Updated table html template

--- scraper/table.go
+++ scraper/table.go
@@ -2,14 +2,22 @@ package main
 
 // TweetTable html template
 var TweetTable = `
-<h1>{{.Handle}}'s Tweets</h1>
+<h1><a href='{{.URL}}'>{{.Handle}}'s Tweets</a></h1>
 <table>
 	<tr style='text-align: left'>
+		<th>Date</th>
 		<th>Text</th>
+		<th>Replies</th>
+		<th>Upvotes</th>
+		<th>Retweets</th>
 	</tr>
-	{{range .List}}
+	{{range .Tweets}}
 	<tr>
+		<td><a href='{{.URL}}'>{{.Date}}</a></td>
 		<td>{{.Text}}</td>
+		<td>{{.Replies}}</td>
+		<td>{{.Upvotes}}</td>
+		<td>{{.Retweets}}</td>
 	</tr>
 	{{end}}
 </table>
