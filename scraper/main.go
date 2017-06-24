021facddd14fef1d737dcf8210d1f7ef0306c920
mlampedx <mlampesd@gmail.com>
2017-06-24 13:45:58 -0700

    Updated scraper

--- scraper/main.go
+++ scraper/main.go
@@ -7,28 +7,45 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
-const tweetClass = "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"
+const (
+	twitterURL      = "https://twitter.com"
+	tweetSelector   = "content"                                                      // Class of Top-Level Tweet Element as well as "You May Also Like" section
+	dateSelector    = "_timestamp js-short-timestamp"                                // Format example: Jun 2
+	textSelector    = "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text" // string
+	replySelector   = "ProfileTweet-action--reply u-hiddenVisually"                  // int
+	upvoteSelector  = "ProfileTweet-action--favorite u-hiddenVisually"               // int
+	retweetSelector = "ProfileTweet-action--retweet u-hiddenVisually"                // int
+	urlSelector     = "tweet-timestamp js-permalink js-nav js-tooltip"               // id
+)
 
-// Tweets type
-type Tweets struct {
+// Profile type
+type Profile struct {
 	Handle string
-	List   []*Tweet
+	URL    string
+	Tweets []*Tweet
 }
 
 // Tweet type
 type Tweet struct {
-	Text string
+	Text     string
+	Date     string
+	URL      string
+	Replies  int
+	Upvotes  int
+	Retweets int
 }
 
 func main() {
-	handle := strings.Join(os.Args[1:], "")
-	url := "https://twitter.com/" + handle
-	s, err := fetch(url)
+	p := &Profile{}
+	p.Handle = strings.Join(os.Args[1:], "")
+	p.URL = twitterURL + "/" + p.Handle
+	s, err := fetch(p.URL)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Scraper failed: Twitter handle not found.\n")
 		os.Exit(1)
@@ -38,8 +55,10 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Scraper failed: %v\n", err)
 		os.Exit(1)
 	}
-	output := &Tweets{handle, scrape(nil, doc)}
-	renderer(output)
+	tweet := &Tweet{}
+	tweets := []*Tweet{}
+	p.Tweets = traverse(doc, tweet, tweets)
+	renderer(p)
 }
 
 func fetch(url string) (bodyString string, err error) {
@@ -58,43 +77,99 @@ func fetch(url string) (bodyString string, err error) {
 	return bodyString, nil
 }
 
-func scrape(list []*Tweet, n *html.Node) []*Tweet {
-	if isTweetNode(n) {
-		// Todo: Escape n.Data string
-		newTweet := &Tweet{n.Data}
-		list = append(list, newTweet)
-	}
+func traverse(n *html.Node, currentTweet *Tweet, tweets []*Tweet) []*Tweet {
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		list = scrape(list, c)
+		currentTweet = buildTweet(c, currentTweet)
+		if isPopulated(currentTweet) {
+			tweets = append(tweets, currentTweet)
+			currentTweet = &Tweet{}
+		}
+		tweets = traverse(c, currentTweet, tweets)
 	}
-	return list
+	return tweets
 }
 
-func isTweetNode(n *html.Node) bool {
-	if n.Type == html.TextNode {
-		// Todo: Figure out how to parse class from html.TextNode or text from html.ElementNode to ensure tweets are parsed
-		// class, ok := getAttribute(n, "class")
-		// if !ok {
-		// 	return false
-		// }
-		// return class == tweetClass
-		return true
+func buildTweet(n *html.Node, t *Tweet) *Tweet {
+	if n.Type == html.ElementNode {
+		class, ok := getAttribute(n, "class")
+		if !ok {
+			return t
+		}
+		switch class {
+		case dateSelector:
+			t.Date = getInnerText(n)
+		case textSelector:
+			t.Text = strings.Trim(getInnerText(n), "")
+		case replySelector:
+			t.Replies = getInnerData(n, "data-tweet-stat-count")
+		case upvoteSelector:
+			t.Upvotes = getInnerData(n, "data-tweet-stat-count")
+		case retweetSelector:
+			t.Retweets = getInnerData(n, "data-tweet-stat-count")
+		case urlSelector:
+			t.URL = getURL(n)
+		}
 	}
-	return false
+	return t
+}
+
+func isPopulated(t *Tweet) bool {
+	return t.Date != "" &&
+		t.Text != "" &&
+		t.Replies != 0 &&
+		t.Upvotes != 0 &&
+		t.Retweets != 0 &&
+		t.URL != ""
 }
 
 func getAttribute(n *html.Node, key string) (attr string, ok bool) {
 	for _, attr := range n.Attr {
-		if attr.Key == key {
+		if compareAttributes(attr.Key, key) {
 			return attr.Val, true
 		}
 	}
 	return "", false
 }
 
-func renderer(tweets *Tweets) {
+func compareAttributes(class, desiredClass string) bool {
+	return class == desiredClass
+}
+
+func getInnerText(n *html.Node) string {
+	c := n.FirstChild
+	if c != nil && c.Type == html.TextNode && len(c.Data) > 0 {
+		return c.Data
+	}
+	return ""
+}
+
+func getInnerData(n *html.Node, attrName string) int {
+	c := n.FirstChild
+	if c != nil {
+		for _, attr := range c.Attr {
+			if attr.Key == attrName {
+				i, err := strconv.Atoi(attr.Val)
+				if err == nil {
+					return i
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func getURL(n *html.Node) string {
+	for _, attr := range n.Attr {
+		if attr.Key == "href" && attr.Val != "" {
+			return twitterURL + attr.Val
+		}
+	}
+	return ""
+}
+
+func renderer(p *Profile) {
 	table := template.Must(template.New("tweettable").Parse(TweetTable))
-	if err := table.Execute(os.Stdout, tweets); err != nil {
+	if err := table.Execute(os.Stdout, p); err != nil {
 		log.Fatal(err)
 	}
 }
